d6dcc9e4f160d3f3faea78a1958916b0d487a105
mohamedisakr <mohamedisakr@gmail.com>
2020-09-16 05:52:53 +0200

    adding selenium and testng

---
+++ src/main/java/lib/Page.java
@@ -0,0 +1,11 @@
+package lib;
+
+import org.openqa.selenium.WebDriver;
+
+public class Page {
+    protected WebDriver driver;
+
+     public Page(WebDriver driver) {
+        this.driver = driver;
+    }
+}
